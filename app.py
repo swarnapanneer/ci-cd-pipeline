226552c205e77302ba04ee6423c472cc95f06180
swarnapanner <swarnapanner24@gmail.com>
2026-03-26 15:43:47 +0530

    again trigger pipeline

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello CI/CD Pipeline v2!"
+    return "Hello CI/CD Pipeline v3!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
