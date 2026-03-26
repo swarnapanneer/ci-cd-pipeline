6c1dca9dfcda164adab3df458461c81dfd578adc
swarnapanner <swarnapanner24@gmail.com>
2026-03-26 14:58:26 +0530

    pipeline test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello DevOps CI/CD Project!"
+    return "Hello CI/CD Pipeline v2!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
